2564d74c212f1f2fee5343c9c11db8544e36b492
Nick Chang <nick.chang@serimaconsulting.com>
2018-11-16 14:29:41 +0100

    add programprogram-instrumental.php

---
+++ registration/includes/program-instrumental.php
@@ -0,0 +1,25 @@
+<input type=hidden name="program" value="Instrumental"><input type=hidden name="source" value="Online Registration">
+
+<div class="reg-section-head">PROGRAM SELECTIONS:</div>
+
+<div class="input-div-radio">
+	<div class="input-label-left-radio">Piano Program I:  July 3-15 </div>
+    <input name="programselection" value="Piano Program I" type="radio" class="input-left-radio"/>
+</div>
+<div class="input-div-radio">
+	<div class="input-label-left-radio">Piano Program II:  July 15-27 </div>
+    <input name="programselection" value="Piano Program II" type="radio" class="input-left-radio"/>
+</div>
+
+<div style="font-size:1px; height:15px;"></div>
+
+<div  class="input-div">I wish to study with: List teachers - up to 6 names</div>
+<div class="input-div">
+    <input type="text" name="studywith" id="studywith" style="width: 520px;" class="input-left"/>
+</div>
+
+
+<div style="font-size:1px; height:15px;"></div>
+To submit audition recordings, please email mp3 file or YouTube link to: <a href="mailto:music@amalfi-festival.org">music@amalfi-festival.org</a>.
+
+<div class="grey-line"></div>
