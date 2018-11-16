7888558e5888633e8f092d90e3f7d195ca82470f
Nick Chang <nick.chang@serimaconsulting.com>
2018-11-16 15:20:11 +0100

    20181116

---
+++ registration/includes/program-visual.php
@@ -0,0 +1,51 @@
+<input type=hidden name="program" value="Visual Arts"><input type=hidden name="source" value="Online Registration">
+<div style="font-size:1px; height:10px;"></div>
+
+<div class="reg-section-head">PROGRAM SELECTIONS:</div>
+<div class="reg-section-head">Session I: July 8 - 17, 2019:  $<?php echo number_format($standardPrice['visual-I'], 0, '.', ',');?> </div>
+
+<div class="input-div-radio">
+	<div class="input-label-left-radio"><strong>Plein Air Painting on the Amalfi Coast</strong><em> (Katherine Knight)</em></div>
+    <input name="programselection" value="S1 Exploring Watercolor on the Amalfi Coast" type="radio" class="input-left-radio"/>
+</div>
+<div class="input-div-radio">
+	<div class="input-label-left-radio"><strong>Ceramic Tile Painting</strong><em> (Robert Devers)</em></div>
+    <input name="programselection" value="S1 Ceramic Sculpture" type="radio" class="input-left-radio"/>
+</div>
+<br>
+<div class="input-div-radio">
+	<div class="input-label-left-radio">Studio classes will add<strong>: $145 </strong>Lab Fee</div>
+    <input name="classextensionS1" value="Studio classes Lab Fee" type="checkbox" class="input-left-radio">
+</div>
+<div>&nbsp;</div>
+<div class="reg-section-head">Session II: July 18 - 27, 2019:  $<?php echo number_format($standardPrice['visual-II'], 0, '.', ',');?></div>
+
+<div class="input-div-radio">
+	<div class="input-label-left-radio"><strong>Ceramic Tile Painting</strong><em> (Robert Devers)</em></div>
+    <input name="programselection" value="S2 Ceramic Tile Painting" type="radio" class="input-left-radio"/>
+</div>
+<!--
+<div class="input-div-radio">
+	<div class="input-label-left-radio"><strong>The Artist's Sketchbook</strong><em> (Rebecca Cross)</em></div>
+    <input name="programselection" value="S2 The Artist Sketchbook" type="radio" class="input-left-radio"/>
+</div>
+-->
+<div class="input-div-radio">
+	<div class="input-label-left-radio"><strong>Photography on the Amalfi Coast</strong><em> (Na’ama Lewin)</em></div>
+    <input name="programselection" value="S2 Photography on the Amalfi Coast" type="radio" class="input-left-radio"/>
+</div>
+<br>
+<div class="input-div-radio">
+	<div class="input-label-left-radio">Studio classes will add<strong>: $145 </strong>Lab Fee</div>
+    <input name="classextensionS2" value="Studio classes Lab Fee" type="checkbox" class="input-left-radio">
+</div>
+<div>&nbsp;</div>
+
+<div style="font-size:1px; height:10px;"></div>
+
+
+<!--<div class="input-div" style="height:40px;">
+	<div class="input-left" style="text-align:left; width:400px;">I wish to study with (name of faculty member): </div>
+    <input type="text"name="studywith" id="studywith" class="input-left" style="top:15px;"/>
+</div>
+ -->
