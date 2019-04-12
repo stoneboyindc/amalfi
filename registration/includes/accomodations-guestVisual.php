12d1b77a0e8199ca51a1b0f89520fdc30622b4ba
Nick Chang <nick.chang@serimaconsulting.com>
2019-04-11 21:45:12 -0400

    add

---
+++ registration/includes/accomodations-guestVisual.php
@@ -0,0 +1,38 @@
+
+<div class="input-div">
+	<div  class="input-label-left-double">Roommate<br/>Request </div>
+    <input type="text" name="roommate" id="roommate" class="input-left"/>
+</div>
+
+<div style="font-size:1px; height:25px;"></div>
+
+<div class="input-div" style="height:15px; line-height:115%;">
+	<div class="input-label-left-radio"><strong>Meal Plan</strong> Dinners: full 3-course meal with wine & mineral water:
+</div>
+</div>
+<div class="input-div">
+	<div class="input-label-left-radio"><strong>Sessions I & II:  $360</strong></div>
+    <input name="mealplan" value="360" type="radio" class="input-left-radio"/>
+</div>
+<div class="input-div">
+	<div class="input-label-left-radio"><strong>Session III:  $480</strong></div>
+    <input name="mealplan" value="480" type="radio" class="input-left-radio"/>
+</div>
+<div style="font-size:1px; height:5px;"></div>
+
+<div style="position:relative; left:15px;">Please note any allergies or health concerns that might affect room and board options:</div>
+<div class="input-div">
+	<div  class="input-label-left">Allergies/Health</div>
+    <input type="text" name="allergies"  id="allergies" class="input-left" style="width:400px;"/>
+</div>
+
+<!--<div class="input-div" style="height:40px;">
+	<div class="input-left" style="text-align:left; width:400px;">I wish to study with (name of faculty member): </div>
+    <input type="text"name="studywith" id="studywith" class="input-left" style="top:15px;"/>
+</div>
+ -->
+
+
+
+<div style="font-size:1px; height:10px;"></div>
+<div class="grey-line"></div>
