86e9504e0956dfd371520c70717d2c8bd37c28e9
Nick Chang <nick.chang@serimaconsulting.com>
2019-04-11 21:42:03 -0400

    add

---
+++ registration/includes/program-guestVisual.php
@@ -0,0 +1,60 @@
+<input type=hidden name="program" value="guestVisual"><input type=hidden name="source" value="Online Registration">
+
+<div class="reg-section-head">PROGRAM SELECTIONS:</div>
+<!--
+<div class="input-div-radio" style="height:100px;">
+	<div class="input-label-left-radio"><strong>Guest – Music Program</strong><em> (Vocal or Instrumental): Provides accommodations for 12-day session in 4-star hotel with festival group plus  all ground transportation. Includes attendance at local and out-of town  concerts, lectures, master classes and workshops; eligible to sign up for all excursions, cooking classes, ceramics and Italian language courses </em></div>
+    <input name="programselection" value="Guest" type="radio" class="input-left-radio"/>
+</div>
+<div class="input-div">
+	<div class="input-label-left-radio"><strong>$2,475</strong></div>
+    <input name="accommodations" value="Hotel" type="radio" class="input-left-radio"/>
+</div>
+
+<div class="input-div-radio" style="left:150px; margin-bottom:0px;">
+	<em>Hotel Supplements:</em> I prefer a 
+</div>
+<div class="input-div-radio" style="left:38px;">
+	<div class="input-label-left-radio">double room: <strong>add $399</strong></strong></div>
+    <input name="roomupgrade" value="Double" type="radio" class="input-left-radio"/>
+</div>
+<div class="input-div-radio" style="left:38px;">
+	<div class="input-label-left-radio">single room:<strong> add $860</strong></div>
+    <input name="roomupgrade" value="Single" type="radio" class="input-left-radio"/>
+</div><div style="font-size:1px; height:15px;"></div>
+-->
+<div class="input-div-radio" style="height:75px;">
+	<strong>Guest – Visual Arts Program</strong><em> Provides accommodations in 4-star hotel with festival group plus  all ground transportation. Includes attendance at local and out-of town concerts, lectures, artists talks, and option to join art trips; eligible to sign up for all excursions, cooking classes, and individual art workshops (ceramics, painting, photography). </em>
+</div>
+<div class="input-div">
+	<div class="input-label-left-radio"><strong>Sessions I & II: $ 2,615</strong></div>
+    <input name="programselection" value="S1" type="radio" class="input-left-radio"/>
+</div>
+<div class="input-div">
+	<div class="input-label-left-radio"><strong>Session III $3,145</strong></div>
+    <input name="programselection" value="S3" type="radio" class="input-left-radio"/>
+</div>
+<div class="input-div-radio" style="left:150px; margin-bottom:0px;">
+	<em>Hotel Supplements:</em> I prefer a 
+</div>
+
+<div class="input-div-radio" style="left:38px;">
+	<div class="input-label-left-radio">single room:<strong> add $365 for Sessions I & II or $ 475 for Session III</strong></div>
+    <input name="roomupgrade" value="Single" type="radio" class="input-left-radio"/>
+</div>
+
+<div style="font-size:1px; height:15px;"></div>
+<!--
+<div style="position:relative; left:110px; top:0px;">Guest/Auditor Session: Piano, Voice, Chamber Music
+(or any period of 12 nights)*</div>
+
+<div class="input-div">
+    <input type="text"name="programspecify" id="programspecify" class="input-left"/>
+</div>
+<div style="position:relative; left:110px; top:0px;"><em>*Please specify if your intended stay will be a different length and we will quote you a daily rate.</em></div>
+-->
+<!--<div class="input-div" style="height:40px;">
+	<div class="input-left" style="text-align:left; width:400px;">I wish to study with (name of faculty member): </div>
+    <input type="text"name="studywith" id="studywith" class="input-left" style="top:15px;"/>
+</div>
+ -->
